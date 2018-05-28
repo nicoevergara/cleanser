5450a1cf9a73a0836e8a9d510b89460c1668b800
ivymarkwell126 <ivy.markwell@utexas.edu>
2018-05-27 23:29:15 -0400

    added is_valid_language?, contains_bad_words? and added 24 different language lists of bad words

--- lib/cleanser.ex
+++ lib/cleanser.ex
@@ -82,9 +82,81 @@ defmodule Cleanser do
     end
   end
 
+  @doc false
   defp disposable_domains do
     Path.join(:code.priv_dir(:cleanser), "disposable_emails.txt")
     |> File.read!
     |> String.split
+  end
+
+  @doc """
+  Bad Words Filter
+
+  Validates a string against a list of bad words in the language chosen.
+
+  ## Examples
+
+      iex> Cleanser.contains_bad_words?("Hello I am normal", "English")
+      :ok
+
+      iex> Cleanser.contains_bad_words?("What the fuck", "english")
+      {:error, "This string contains bad words"}
+
+      iex> Cleanser.contains_bad_words?("ok ok 挿入 ok ok", "japanese")
+      {:error, "This string contains bad words"}
+  """
+
+  def contains_bad_words?(string, language) do
+    is_valid_language?(language)
+    words = String.downcase(string)
+            |>String.split
+            |>MapSet.new
+    is_valid = bad_words(language)
+              |> MapSet.new
+              |> MapSet.disjoint?(words)
+    case is_valid do
+      true ->
+        :ok
+      false ->
+        {:error, "This string contains bad words"}
+    end
+  end
+
+  defp bad_words(language) do
+    Path.join(:code.priv_dir(:cleanser), "bad_words/#{language}")
+    |> File.read!
+    |> String.downcase()
+    |> String.split
+  end
+
+  @doc """
+  Language Validation
+
+  Validates a language against a list of available languages.
+
+  ## Examples
+
+      iex> Cleanser.is_valid_language?("dutch")
+      :ok
+
+      iex> Cleanser.is_valid_language?("Gibberish")
+      {:error, "gibberish is either not a valid language or not available in this package"}
+
+  """
+
+  def is_valid_language?(language) do
+    language = String.downcase(language)
+    valid_language = ["arabic", "chinese", "czech", "danish", "dutch", "english", "esperanto", "finnish", "french",
+      "german", "hindi", "hungarian", "italian", "japanese", "klingon", "korean", "norwegian", "persian", "polish",
+      "portuguese", "russian", "spanish", "swedish", "thai", "turkish"]
+                    |> MapSet.new
+                    |> MapSet.member?(language)
+
+    case valid_language do
+      true ->
+        :ok
+      false ->
+        {:error, "#{language} is either not a valid language or not available in this package"}
+    end
   end
 end
